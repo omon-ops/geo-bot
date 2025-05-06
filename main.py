42fb1e788dc71606cbc9b7f0d58b633f75d66da2
omon-ops <65026263+omon-ops@users.noreply.github.com>
2025-05-06 17:41:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import os
+from discord.ext import commands
+import discord
 
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot online como {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+bot.run(os.environ["DISCORD_TOKEN"])
